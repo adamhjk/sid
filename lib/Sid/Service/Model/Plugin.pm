d59c1be8e3dbafd5b4784666d23425eeb18a7089
Adam Jacob <adam@hjksolutions.com>
2008-12-02 12:37:53 -0800

    Adding the apache license

--- lib/Sid/Service/Model/Plugin.pm
+++ lib/Sid/Service/Model/Plugin.pm
@@ -3,6 +3,24 @@
 # Created by: Adam Jacob, Marchex, <adam@marchex.com>
 #
 # $Id: DB.pm,v 1.1.2.1 2006/07/07 02:23:11 adam Exp $
+#
+# Author:: Adam Jacob (<adam@opscode.com>)
+# Copyright:: Copyright (c) 2008 Adam Jacob
+# License:: Apache License, Version 2.0
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#     http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
 
 use strict;
 use warnings;
